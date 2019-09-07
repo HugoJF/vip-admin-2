3ac0d5ab1889fd259d6b1d460162e1bc899cbcbf
vagrant <vagrant@homestead>
2019-09-06 23:40:08 -0400

    added some breadcrumbs

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -2,11 +2,41 @@
 
 // Login
 Breadcrumbs::for ('home', function ($trail) {
-	$trail->push('Home', route('home'));
+	$trail->push('Início', route('home'));
 });
 
-// Home > Servers
-Breadcrumbs::for ('servers.index', function ($trail) {
+// Settings
+Breadcrumbs::for ('settings', function ($trail) {
 	$trail->parent('home');
-	$trail->push('Servers', route('servers.index'));
+	$trail->push('Configurações', route('settings'));
+});
+
+// Settings
+Breadcrumbs::for ('affiliates.index', function ($trail) {
+	$trail->parent('home');
+	$trail->push('Afiliados', route('affiliates.index'));
+});
+
+// Home > Orders
+Breadcrumbs::for ('orders.index', function ($trail) {
+	$trail->parent('home');
+	$trail->push('Pedidos', route('orders.index'));
+});
+
+// Home > Tokens
+Breadcrumbs::for ('tokens.index', function ($trail) {
+	$trail->parent('home');
+	$trail->push('Tokens', route('tokens.index'));
+});
+
+// Home > Tokens > Create
+Breadcrumbs::for ('tokens.create', function ($trail) {
+	$trail->parent('tokens.index');
+	$trail->push('Criando tokens', route('tokens.create'));
+});
+
+// Home > Users
+Breadcrumbs::for ('users.index', function ($trail) {
+	$trail->parent('home');
+	$trail->push('Usuários', route('users.index'));
 });
