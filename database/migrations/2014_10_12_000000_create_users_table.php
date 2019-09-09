8006f0ac95231340e97c06ebcd33630ab561b690
vagrant <vagrant@homestead>
2019-09-09 01:34:47 -0400

    merged aditional migrations into users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,39 +6,46 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function (Blueprint $table) {
+			$table->bigIncrements('id');
 
-            $table->string('name')->nullable();
-            $table->string('username');
-            $table->string('tradelink')->nullable();
+			$table->string('name')->nullable();
+			$table->string('username');
+
+			$table->string('tradelink')->nullable();
 
 			$table->string('steamid');
 			$table->string('avatar')->nullable();
 
 			$table->boolean('admin')->default(false);
+			$table->boolean('terms')->default(false);
 
 			$table->string('email')->unique()->nullable();
 
-			$table->rememberToken();
-            $table->timestamps();
-        });
-    }
+			$table->string('affiliate_code')->nullable();
+	
+			$table->unsignedInteger('referrer_id')->nullable();
+			$table->dateTime('referred_at')->nullable();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+			$table->rememberToken();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('users');
+	}
 }
