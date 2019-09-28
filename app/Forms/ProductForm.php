a19ab111e84d9f71d96ac4a4ea7cde7d49efde08
Hugo Jeller <hugo_jeller@hotmail.com>
2019-09-28 02:38:23 -0400

    added help blocks and labels to forms

--- app/Forms/ProductForm.php
+++ app/Forms/ProductForm.php
@@ -2,31 +2,41 @@
 
 namespace App\Forms;
 
-use Kris\LaravelFormBuilder\Form;
-
 class ProductForm extends Form
 {
 	public function buildForm()
 	{
-		$this->add('title', 'text');
+		$this->add('title', 'text', [
+			'label'      => 'Título',
+			'help_block' => $this->buildHelpBlock(''),
+		]);
 		$this->add('duration', 'number', [
-			'attr' => [
+			'label'      => 'Duração',
+			'help_block' => $this->buildHelpBlock('Tempo em dias da duração do VIP'),
+			'attr'       => [
 				'min' => 0,
 				'max' => 90,
 			],
 		]);
 		$this->add('cost', 'number', [
-			'attr' => [
+			'label'      => 'Custo',
+			'help_block' => $this->buildHelpBlock('Custo atual em centavos do período de VIP'),
+			'attr'       => [
 				'min' => 0,
 				'max' => 5000,
 			],
 		]);
 		$this->add('original_cost', 'number', [
-			'attr' => [
-				'min'  => 0,
-				'max'  => 5000,
+			'label'      => 'Custo original',
+			'help_block' => $this->buildHelpBlock('Custo original em centavos do período de VIP (usado para comparar desconto atual)'),
+			'attr'       => [
+				'min' => 0,
+				'max' => 5000,
 			],
 		]);
-		$this->add('description', 'textarea');
+		$this->add('description', 'textarea', [
+			'label'      => 'Descrição do VIP',
+			'help_block' => $this->buildHelpBlock('Descrição do <i>card</i> do VIP'),
+		]);
 	}
 }
--- app/Forms/UserSettingsForm.php
+++ app/Forms/UserSettingsForm.php
@@ -9,18 +9,24 @@ class UserSettingsForm extends Form
 		$user = $this->getData('user');
 
 		$this->add('tradelink', 'text', [
+			'label'      => 'Link de troca',
 			'help_block' => $this->buildHelpBlock('Esse é o link que utilizamos para enviar os pedidos de troca. Você pode achar sua URL <strong><u><a href="https://steamcommunity.com/id/me/tradeoffers/privacy#trade_offer_access_url">clicando aqui</a></u></strong>. (obrigatório apenas na compra com itens de CS:GO)'),
 		]);
 		$this->add('email', 'text', [
+			'label'      => 'E-mail',
 			'help_block' => $this->buildHelpBlock('Utilizaremos esse email para enviar <strong>promoções, descontos e notificações</strong> da sua conta no VIP-Admin. (recomendado mas opcional)'),
 		]);
 
 		if (config('vip-admin.enforce-terms', false))
-			$this->add('terms', 'checkbox');
+			$this->add('terms', 'checkbox', [
+				'label'      => 'Concordar com os termos',
+				'help_block' => $this->buildHelpBlock('Consulte nosso termos de serviço <a href="' . route('terms') . '">clicando aqui.</a>'),
+			]);
 
 		if ($user && $user->affiliate)
 			$this->add('affiliate_code', 'text', [
-				'help_block' => $this->buildHelpBlock('Código que será utilizado no seu link de afiliado. Exemplo: <strong>' . route('affiliate', 'seucodigoaqui') . '</strong>')
+				'label'      => 'Código de afiliado',
+				'help_block' => $this->buildHelpBlock('Código que será utilizado no seu link de afiliado. Exemplo: <strong>' . route('affiliate', 'seucodigoaqui') . '</strong>'),
 			]);
 	}
 }
