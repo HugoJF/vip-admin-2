35a6407adab496ba3e8133d1ffaae92cf997a841
Hugo Jeller <hugo_jeller@hotmail.com>
2020-01-21 00:21:07 -0400

    Fixed TokenController not returning redirects

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -16,102 +16,105 @@ use Kris\LaravelFormBuilder\FormBuilder;
 
 class TokenController extends Controller
 {
-	public function index()
-	{
-		$user = auth()->user();
+    public function index()
+    {
+        $user = auth()->user();
 
-		if ($user->admin) {
-			$tokens = Token::query();
-		} else {
-			$tokens = $user->tokens();
-		}
+        if ($user->admin) {
+            $tokens = Token::query();
+        } else {
+            $tokens = $user->tokens();
+        }
 
-		$tokens = $tokens->with(['user'])->get();
+        $tokens = $tokens->with(['user'])->get();
 
-		return view('tokens.index', compact('tokens'));
-	}
+        return view('tokens.index', compact('tokens'));
+    }
 
-	public function show(Token $token)
-	{
-		return view('tokens.show', compact('token'));
-	}
+    public function show(Token $token)
+    {
+        return view('tokens.show', compact('token'));
+    }
 
-	public function use(Token $token)
-	{
-		if ($token->order !== null)
-			throw new AlreadyUsedTokenException('Token already used.');
+    public function use(Token $token)
+    {
+        if ($token->order !== null)
+            throw new AlreadyUsedTokenException('Token already used.');
 
-		if ($token->expires_at && $token->expires_at->isPast())
-			throw new TokenExpiredException('Expired tokens cannot be used');
+        if ($token->expires_at && $token->expires_at->isPast())
+            throw new TokenExpiredException('Expired tokens cannot be used');
 
-		// TODO: move to service layer
-		DB::beginTransaction();
+        // TODO: move to service layer
+        DB::beginTransaction();
 
         $order = new Order();
 
-		 try {
+        try {
 
-             $order->id = "to$token->id";
-             $order->duration = $token->duration;
+            $order->id = "to$token->id";
+            $order->duration = $token->duration;
 
-             $order->paid = true;
-             $order->canceled = false;
+            $order->paid = true;
+            $order->canceled = false;
 
-             $order->user()->associate(auth()->user());
-             $token->order()->associate($order);
+            $order->user()->associate(auth()->user());
+            $token->order()->associate($order);
 
-             $order->save();
-             $token->save();
-         } catch (Exception $e) {
-		     DB::rollBack();
-         }
+            $order->save();
+            $token->save();
+        } catch (Exception $e) {
+            DB::rollBack();
+        }
 
-         DB::commit();
+        DB::commit();
 
-		flash()->success('Token registrado com sucesso!');
+        flash()->success('Token registrado com sucesso!');
 
-		return redirect()->route('orders.show', $order);
-	}
+        return redirect()->route('orders.show', $order);
+    }
 
-	public function create(FormBuilder $builder)
-	{
-		$form = $builder->create(TokenForm::class, [
-			'method' => 'POST',
-			'url'    => route('tokens.store'),
-		]);
+    public function create(FormBuilder $builder)
+    {
+        $form = $builder->create(TokenForm::class, [
+            'method' => 'POST',
+            'url'    => route('tokens.store'),
+        ]);
 
-		return view('form', [
-			'title'       => 'Criando novo token',
-			'form'        => $form,
-			'submit_text' => 'Criar',
-		]);
-	}
+        return view('form', [
+            'title'       => 'Criando novo token',
+            'form'        => $form,
+            'submit_text' => 'Criar',
+        ]);
+    }
 
-	public function store(TokenStoreRequest $request)
-	{
-		$token = new Token();
+    public function store(TokenStoreRequest $request)
+    {
+        $token = new Token();
 
-		$token->fill($request->validated());
-		$token->reason()->associate(auth()->user());
+        $token->fill($request->validated());
+        $token->reason()->associate(auth()->user());
 
-		$token->save();
+        $token->save();
 
-		return redirect()->route('tokens.show', $token);
-	}
+        return redirect()->route('tokens.show', $token);
+    }
 
-	public function update(TokenUpdateRequest $request, Token $token)
-	{
-		$token->fill($request->validated());
+    public function update(TokenUpdateRequest $request, Token $token)
+    {
+        $token->fill($request->validated());
 
-		$token->save();
+        $token->save();
 
-		return $token;
-	}
+        return redirect()->route('tokens.show', $token);
+    }
 
-	public function destroy(Token $token)
-	{
-		$token->delete();
+    public function destroy(Token $token)
+    {
+        $token->delete();
 
-		return $token;
-	}
+        $id = e($token->id);
+        flash()->success("Token <strong>$id</strong> removido com sucesso!");
+
+        return redirect()->route('tokens.index');
+    }
 }
