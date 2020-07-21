52c58d6328c2d30cad5d5a3ddf60b80cbaf49a33
Hugo Jeller <hugo_jeller@hotmail.com>
2020-07-21 22:26:37 +0000

    Migrated alerts to model-warnings

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,19 +91,15 @@
                 </div>
 
                 @auth
-                    @php
-                        $alerts = auth()->user()->getAlerts();
-                    @endphp
-
-                    @if($alerts->count() !== 0)
+                    @if(($warnings = auth()->user()->warnings()) && !empty($warnings))
                         <h6 class="flex justify-between items-center px-3 mt-8 mb-4 uppercase font-normal tracking-wider text-gray-700">
                             <span>Alertas</span>
                             <span class="ml-4 mt-px flex-grow border-b border-dashed border-gray-800"></span>
                         </h6>
 
                         <ul class="flex flex-col items-center">
-                            @foreach ($alerts as $alert)
-                                <li><a href="{{ $alert['url'] }}" class="badge badge-{{ $alert['level'] }}">{{ $alert['message'] }}</a></li>
+                            @foreach ($warnings as $warning)
+                                <li><a href="{{ $warning['url'] }}" class="badge badge-{{ $warning['level'] }}">{{ $warning['message'] }}</a></li>
                             @endforeach
                         </ul>
                     @endif
