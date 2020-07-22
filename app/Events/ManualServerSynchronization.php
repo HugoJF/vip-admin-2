c728842dd14e810f2571dfdba1a078d2fcfbef28
Hugo Jeller <hugo_jeller@hotmail.com>
2020-07-22 02:30:24 +0000

    Removed unecessary construct boilerplate

--- app/Events/ManualServerSynchronization.php
+++ app/Events/ManualServerSynchronization.php
@@ -12,16 +12,6 @@ class ManualServerSynchronization
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Get the channels the event should broadcast on.
      *
      * @return \Illuminate\Broadcasting\Channel|array
